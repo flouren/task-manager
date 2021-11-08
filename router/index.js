e3f1c3714336a837b619bba57da98f9984bac9bf
flouren <bbaal@wp.pl>
2021-11-08 20:46:24 +0100

    clearing routes old functionality

--- router/index.js
+++ router/index.js
@@ -8,40 +8,6 @@ router.get('/', (req, res, next) => {
     })
 });
 
-/**
- * GET all tasks
- */
 
-router.get('/api/v1/tasks', (req, res, next) => {
-    res.send('route for all tasks')
-});
-
-/**
- * create new task
- */
-router.post('/api/v1/tasks', (req, res, next) => {
-    res.send('post route')
-});
-
-/**
- * get single task!
- */
- router.get('/api/v1/tasks/:id', (req, res, next) => {
-    
-});
-
-/**
- * update task
- */
- router.patch('/api/v1/tasks/:id', (req, res, next) => {
-    
-});
-
-/**
- * delete task
- */
- router.delete('/api/v1/tasks/:id', (req, res, next) => {
-    
-});
 
 module.exports = router;
