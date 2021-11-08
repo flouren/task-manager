f5fb0f05bbd6c8b414d6ff7fd4711d760ccaa55c
flouren <bbaal@wp.pl>
2021-11-08 21:38:32 +0100

    recreating responds for testing

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,25 +4,31 @@ const getAllTasks = (req,res) => {
 
 
 const createTask = (req,res) => {
-    res.send('create task')
+    res.json(req.body)
 }
 
 
 
 const getSingleTask = (req,res) => {
-    res.send('single task task')
+    res.json(req.params)
 }
 
 
 
 const updateTask = (req,res) => {
-    res.send('update one task')
+    res.json({
+        'id':req.params,
+        'message':'update one task'
+    })
 }
 
 
 
 const deleteTask = (req,res) => {
-    res.send('delete one task')
+    res.json({
+        'message':`delete one task with id`,
+        'id':req.params
+    })
 }
 
 
