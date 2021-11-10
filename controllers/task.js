47de256f9b61236bf66c535daa0ead657225d304
flouren <bbaal@wp.pl>
2021-11-10 22:48:52 +0100

    added full controller to get all tasks to create task and find single

--- controllers/task.js
+++ controllers/task.js
@@ -1,21 +1,47 @@
 const Task = require('../models/Tasks')
 
-const getAllTasks = (req,res) => {
+const getAllTasks = async (req,res) => {
     // Task.find()
     // then(e=>console.log(e))
-    res.send('all task')
+    try {
+        const tasks = await Task.find()
+        res.status(200).json({tasks})  
+    } catch (error) {
+        res.status(500).json({msg:error.message})
+    }
+        // .then()
+        // .catch()
+
+    //res.send('all task')
 }
 
 
 const createTask = async (req,res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task})  
+    } catch (error) {
+        res.status(500).json({msg:error.message})
+    }
+
 }
 
 
 
-const getSingleTask = (req,res) => {
-    res.json(req.params)
+const getSingleTask = async (req,res) => {
+
+    try {
+        const {id:taskID} = req.params 
+        const task = await Task.findOne({_id:taskID})
+        res.status(200).json({task})  
+        if(!task){
+            return res.status(404),json({msg:`No task with id: ${taskID}`})
+        }
+    } catch (error) {
+        res.status(500).json({msg:error.message})
+    }
+    //res.json(req.params)
 }
 
 
