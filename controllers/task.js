2895d296ecabfedf5e6c31d2384f81f27a997e9c
flouren <bbaal@wp.pl>
2021-11-14 14:07:01 +0100

    refactoring controllers to asyncWrapper done!

--- controllers/task.js
+++ controllers/task.js
@@ -6,51 +6,41 @@ const asyncWrapper = require('../middleware/async')
 
 const getAllTasks = asyncWrapper(async (req,res) => {
     
-    
         const tasks = await Task.find()
         // res.status(200).json({tasks})  
         // res.status(200).json({tasks,amount:tasks.length})  
         res.status(200).json({success:true,data:{tasks,amount:tasks.length}})  
 
-
-    
-        
 })
 
 
-const createTask = async (req,res) => {
+const createTask = asyncWrapper(async (req,res) => {
 
-    try {
+    
         const task = await Task.create(req.body)
         res.status(201).json({task})  
-    } catch (error) {
-        res.status(500).json({msg:error.message})
-    }
+    
 
-}
+})
 
 
 
-const getSingleTask = async (req,res) => {
+const getSingleTask = asyncWrapper(async (req,res) => {
 
-    try {
+    
         const {id:taskID} = req.params 
         const task = await Task.findOne({_id:taskID})
         if(!task){
             return res.status(404).json({msg:`No task with id: ${taskID}`})
         }
         res.status(200).json({task})
-    } catch (error) {
-        res.status(500).json({msg:error.message})
-    }
     
-}
+})
 
 
 
-const updateTask = async (req,res) => {
+const updateTask = asyncWrapper(async (req,res) => {
     
-    try {
         const {id:taskID} = req.params
         const task = await Task.findOneAndUpdate({_id:taskID},req.body,
             {
@@ -64,17 +54,12 @@ const updateTask = async (req,res) => {
         }
         res.status(200).json({task})
 
-    } catch (error) {
-        res.status(500).json({msg:error.message})
-    }
-
-}
+})
 
 
 
-const deleteTask = async (req,res) => {
+const deleteTask = asyncWrapper(async (req,res) => {
 
-    try {
         const {id:taskID} = req.params 
         const task = await Task.findOneAndDelete({_id:taskID})
         if(!task){
@@ -82,11 +67,7 @@ const deleteTask = async (req,res) => {
         }
         res.status(200).json({task})
 
-    } catch (error) {
-        res.status(500).json({msg:error.message})
-    }
-
-}
+})
 
 
 
